c584f84b42b30ba7856d7e6e0a5ecb32102a29c2
DmitryShcherba <debaucep@yandex.ru>
2023-02-21 22:37:58 +0300

    Add second VC, Transfer BMI -> secondVC into Label. Second VC is done programmatically

--- BMI Calculator/Controllers/ViewController.swift
+++ BMI Calculator/Controllers/ViewController.swift
@@ -9,12 +9,56 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    
+    @IBOutlet weak var heightLabel: UILabel!
+    @IBOutlet weak var weightLabel: UILabel!
+    
+    var height = 1.5 // double
+    var weight = 100  // int
+    var bmi = 0.0 // double
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    
+    @IBAction func heightSliderChanged(_ sender: UISlider) {
+        //print (String(format: "%.2f", sender.value))
+        
+        heightLabel.text = String(format: "%.2f", sender.value) + "m"
+        
+        height = Double(String(format: "%.2f", sender.value))!
+        
+//        heightLabel.text = "m"
+    }
+    
+    @IBAction func weightSliderChanged(_ sender: UISlider) {
+        //print (String(format: "%.0f", sender.value))
+        weightLabel.text = (String(format: "%.0f", sender.value)) + "kg"
+        
+        weight = Int(String(format: "%.0f", sender.value))!
+    }
+    
+    @IBAction func calculateBtnPressed(_ sender: UIButton) {
+        print ("current height is \(height) meters")
+        print ("current weight is \(weight) kilogramms")
+        
+        bmi = Double (weight) / (height * height)
+        print ("BMI score is \(bmi)")
+        
+        let secondVC = SecondVC ()
+        
+        secondVC.bmiValue = String(bmi)
+      
+        
+        self.present(secondVC, animated: true, completion: nil)
+        
+    }
+    
+    
 }
+
 
---
+++ BMI Calculator/TestFile.swift
@@ -0,0 +1,9 @@
+//
+//  TestFile.swift
+//  BMI Calculator
+//
+//  Created by dmitry shcherba on 17.02.2023.
+//  Copyright © 2023 Angela Yu. All rights reserved.
+//
+
+import Foundation
