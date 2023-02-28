437f66c9e1728a6a9dd98a66b808f41d96d99fd3
DmitryShcherba <debaucep@yandex.ru>
2023-02-28 23:47:46 +0300

    xcode lags again?

--- BMI Calculator/Controllers/ResultsViewController.swift
+++ BMI Calculator/Controllers/ResultsViewController.swift
@@ -30,6 +30,7 @@ class ResultsViewController: UIViewController {
 
     @IBAction func recalculatePressed(_ sender: UIButton) {
         self.dismiss(animated: true)
+        print ("checked")
     }
 
     
